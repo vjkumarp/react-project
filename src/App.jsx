040d88c40dd4b90cb7b3d78e3ff807f0990ce23e
Vijay Kumar P <iamvj1993kumar@gmail.com>
2024-10-01 20:53:29 +0530

    Project Update

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { React, useState } from "react";
+import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
+
+function App() {
+  const [selectTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Componenets
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {/* {!selectTopic ? (
+            <p>Please select a topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectTopic].title}</h3>
+              <p>{EXAMPLES[selectTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
+          {!selectTopic && <p>Please select a topic</p>}
+          {selectTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectTopic].title}</h3>
+              <p>{EXAMPLES[selectTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectTopic].code}</code>
+              </pre>
+            </div>
+          )}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
